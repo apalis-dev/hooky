3d002a069ff2a9ace3f31ba0323aca9150da1403
Nzai Kilonzo <nzaih18@gmail.com>
2026-04-10 15:03:57 +0300

    fix

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -3,9 +3,5 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-<<<<<<< HEAD
 	plugins: [reactRouter(), tsconfigPaths()],
-=======
-  plugins: [reactRouter(), tsconfigPaths()],
->>>>>>> 3c2136e (refactor: add shared routes for the dashboard and change)
 });
